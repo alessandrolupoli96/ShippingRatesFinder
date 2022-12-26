1df5f1bf39a4439331d727e0b578390615750294
hawkast <116033356+hawkast@users.noreply.github.com>
2022-12-26 19:53:04 +0100

    Add files via upload

---
+++ ShippingRatesFinderFE/js/jscript.js
@@ -0,0 +1,10 @@
+
+    function visualizza(id){
+    if (document.getElementById){
+    if(document.getElementById(id).style.display === 'none'){
+    document.getElementById(id).style.display = 'block';
+}else{
+    document.getElementById(id).style.display = 'none';
+}
+}
+}
